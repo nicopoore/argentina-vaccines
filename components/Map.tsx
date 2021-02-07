703754d00ddb96a6436db158e2228f5e3c590f34
Nicolás Poore <nicolaspoore@gmail.com>
2021-02-07 11:01:36 -0300

    Map and tooltip added

---
+++ components/Map.tsx
@@ -0,0 +1,45 @@
+import { Box, Grid } from "@material-ui/core";
+import React, { memo } from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Sphere,
+} from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/argentina/argentina-provinces.json";
+
+const Map = ({ setTooltipContent }): JSX.Element => {
+  const size = 300;
+  return (
+    <Box width="100%" height="100vh" overflow="hidden">
+      <ComposableMap
+        projection="geoMercator"
+        projectionConfig={{ scale: 450, center: [-62, -50] }}
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                fill="#EAEAEC"
+                geography={geo}
+                stroke="#000"
+                onMouseEnter={() => {
+                  const { NAME_1 } = geo.properties;
+                  setTooltipContent(NAME_1);
+                }}
+                onMouseLeave={() => {
+                  setTooltipContent("");
+                }}
+              ></Geography>
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+    </Box>
+  );
+};
+
+export default memo(Map);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,41 @@
 import Head from "next/head";
-import { Box } from "@material-ui/core";
+import { Box, Tooltip, Typography } from "@material-ui/core";
+import { Map } from "../components";
+import ReactTooltip from "react-tooltip";
+import { useState } from "react";
 
 const Home = (): JSX.Element => {
-  return <Box>hello</Box>;
+  const [content, setContent] = useState("");
+  return (
+    <>
+      <Head>
+        <title>Argentina Sputnik-V</title>
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
+      <Map setTooltipContent={setContent} />
+      <Box
+        position="absolute"
+        top="50%"
+        style={{ transform: "translateY(-50%)" }}
+        left={100}
+      >
+        <Typography variant="h5">
+          Vacunas Sputnik-V aplicadas por provincia
+        </Typography>
+        <Typography>
+          Pasá el mouse por cada provincia para ver los datos
+        </Typography>
+      </Box>
+      <Box
+        position="absolute"
+        top="50%"
+        style={{ transform: "translateY(-50%)" }}
+        right={400}
+      >
+        <Typography variant="h5">{content}</Typography>
+      </Box>
+    </>
+  );
 };
 
 export default Home;
