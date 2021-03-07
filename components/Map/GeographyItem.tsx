1759f41aa5c3b0a0432df46b563fe651f8899e64
Nicolás Poore <nicolaspoore@gmail.com>
2021-03-07 16:02:17 -0300

    Map clickaway listener added

--- components/Map/GeographyItem.tsx
+++ components/Map/GeographyItem.tsx
@@ -16,34 +16,43 @@ const GeographyItem: React.FC<Props> = ({ setSelectedProvince, caba }): JSX.Elem
   const selectedProvince = useContext(SelectionContext);
 
   return (
-    <Geographies geography={geoUrl}>
-      {({ geographies }) =>
-        geographies.filter(filter).map(geo => {
-          const displayProvince =
-            geo.properties.NAME_1 === 'Ciudad de Buenos Aires' ? 'CABA' : geo.properties.NAME_1;
-          const isSelected = displayProvince === selectedProvince ? true : false;
-          return (
-            <Geography
-              key={geo.rsmKey}
-              fill={isSelected ? '#8cb4f5' : '#EAEAEC'}
-              geography={geo}
-              stroke="#555"
-              strokeWidth={caba && 8}
-              style={{
-                default: { outline: 'none' },
-                hover: { fill: isSelected ? '#5d91e3' : '#DADADC', outline: 'none' },
-                pressed: { fill: isSelected ? '#8cb4f5' : '#DADADC', outline: 'none' },
-              }}
-              onClick={() => {
-                isSelected
-                  ? setSelectedProvince('Argentina')
-                  : setSelectedProvince(displayProvince);
-              }}
-            />
-          );
-        })
-      }
-    </Geographies>
+    <>
+      <rect
+        fill="transparent"
+        height="100%"
+        width="100%"
+        onClick={() => setSelectedProvince('Argentina')}
+      />
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.filter(filter).map(geo => {
+            const displayProvince =
+              geo.properties.NAME_1 === 'Ciudad de Buenos Aires' ? 'CABA' : geo.properties.NAME_1;
+            const isSelected = displayProvince === selectedProvince ? true : false;
+            // const d = data.
+            return (
+              <Geography
+                key={geo.rsmKey}
+                fill={isSelected ? '#8cb4f5' : '#EAEAEC'}
+                geography={geo}
+                stroke="#555"
+                strokeWidth={caba && 8}
+                style={{
+                  default: { outline: 'none' },
+                  hover: { fill: isSelected ? '#5d91e3' : '#DADADC', outline: 'none' },
+                  pressed: { fill: isSelected ? '#8cb4f5' : '#DADADC', outline: 'none' },
+                }}
+                onClick={() => {
+                  isSelected
+                    ? setSelectedProvince('Argentina')
+                    : setSelectedProvince(displayProvince);
+                }}
+              />
+            );
+          })
+        }
+      </Geographies>
+    </>
   );
 };
 
