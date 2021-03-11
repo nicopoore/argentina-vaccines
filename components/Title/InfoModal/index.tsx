bc2fa681ead576052a8c8bfd9aa3e16240aa939e
Nicolás Poore <nicolaspoore@gmail.com>
2021-03-11 15:08:24 -0300

    Updated title, added info modal

---
+++ components/Title/InfoModal/index.tsx
@@ -0,0 +1,59 @@
+import {
+  Modal,
+  Text,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Box,
+  Link,
+  chakra,
+} from '@chakra-ui/react';
+import React from 'react';
+import { outsideLinks, instructions } from './data.json';
+
+interface Props {
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+const InfoModal: React.FC<Props> = (props): JSX.Element => {
+  return (
+    <Modal isOpen={props.isOpen} size="2xl" onClose={props.handleClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Argentina Vacunada</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Box mb={3}>
+            <Text fontWeight="bold">Instrucciones</Text>
+            {instructions.map(instructionsItem => (
+              <Text mt={2}>
+                {instructionsItem[0]}
+                <chakra.span color="cyan.400" fontWeight="bold">
+                  {instructionsItem[1]}
+                </chakra.span>{' '}
+                {instructionsItem[2]}
+              </Text>
+            ))}
+          </Box>
+          {outsideLinks.map(outsideLinksObject => (
+            <Box mb={3}>
+              <Text fontWeight="bold">{outsideLinksObject.name}</Text>
+              {outsideLinksObject.links.map(outsideLink => (
+                <Text>
+                  <Link isExternal color="teal.400" href={outsideLink.href}>
+                    {outsideLink.name}
+                  </Link>
+                </Text>
+              ))}
+            </Box>
+          ))}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default InfoModal;
--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Stack,
+  Button,
+  ButtonGroup,
+  Flex,
+  IconButton,
+  Text,
+  Link,
+  Tooltip,
+} from '@chakra-ui/react';
+import { QuestionOutlineIcon } from '@chakra-ui/icons';
+import InfoModal from './InfoModal/index';
 
-const Title: React.FC = (): JSX.Element => (
-  <Box pl={4} w="100%">
-    <Text as="h1" fontSize="3xl" m={2} mb={-3}>
-      Argentina vacunada
-    </Text>
-  </Box>
-);
+const Title: React.FC = (): JSX.Element => {
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
+
+  const handleOpen = (): void => {
+    setInfoIsOpen(() => true);
+  };
+
+  const handleClose = (): void => {
+    setInfoIsOpen(() => false);
+  };
+
+  return (
+    <>
+      <Flex alignItems="center" justify="space-between" m="auto" w="90%">
+        <Text as="h1" fontSize="3xl" fontWeight="bold" m={2}>
+          Argentina vacunada
+        </Text>
+        <ButtonGroup isAttached>
+          <Button>Vista simplificada</Button>
+          <Button>Vista completa</Button>
+        </ButtonGroup>
+        <Stack alignItems="center" direction="row">
+          <Link isExternal color="gray.500" href="https://github.com/nicopoore/argentina-vaccines">
+            Creado por Nicolás Poore
+          </Link>
+          <Tooltip label="Ayuda">
+            <IconButton aria-label="Help" icon={<QuestionOutlineIcon />} onClick={handleOpen} />
+          </Tooltip>
+        </Stack>
+      </Flex>
+      <InfoModal handleClose={handleClose} isOpen={infoIsOpen} />
+    </>
+  );
+};
 
 export default Title;
