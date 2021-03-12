6b1cbb8bb8caa222c77a804b92da6a04dc357fa3
Nicolás Poore <nicolaspoore@gmail.com>
2021-03-11 23:48:34 -0300

    Meta last fix hopefully

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 export { default as Title } from './Title'
 export { default as Map } from './Map'
 export { default as Data } from './Data'
-export { default as Meta } from './Meta'
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Stack, Text } from '@chakra-ui/react';
 import useSWR, { mutate } from 'swr';
 
 // Components
-import { Map, Data, Title, Meta } from '../components';
+import { Map, Data, Title } from '../components';
 
 // Utils
 import { fetcher, postCurrentData } from '../utils/functions';
@@ -27,39 +27,33 @@ const Home = (): JSX.Element => {
 
   if (error)
     return (
-      <>
-        <Meta />
-        <Stack m="auto" w={{ base: '100%', xl: '80%' }}>
-          <Title isSimplified={isSimplified} setIsSimplified={setIsSimplified} />
-          <Stack direction="row" wrap={{ base: 'wrap', md: 'nowrap' }}>
-            <SelectionContextProvider selectedProvince={selectedProvince}>
-              <Map setSelectedProvince={setSelectedProvince} />
-            </SelectionContextProvider>
-            <Stack alignItems="center" flexGrow={1} justify="center">
-              <Text>Error al buscar datos</Text>
-              <Text>Error: {error.name}</Text>
-              <Text>Mensaje: {error.message}</Text>
-            </Stack>
+      <Stack m="auto" w={{ base: '100%', xl: '80%' }}>
+        <Title isSimplified={isSimplified} setIsSimplified={setIsSimplified} />
+        <Stack direction="row" wrap={{ base: 'wrap', md: 'nowrap' }}>
+          <SelectionContextProvider selectedProvince={selectedProvince}>
+            <Map setSelectedProvince={setSelectedProvince} />
+          </SelectionContextProvider>
+          <Stack alignItems="center" flexGrow={1} justify="center">
+            <Text>Error al buscar datos</Text>
+            <Text>Error: {error.name}</Text>
+            <Text>Mensaje: {error.message}</Text>
           </Stack>
         </Stack>
-      </>
+      </Stack>
     );
 
   return (
-    <>
-      <Meta />
-      <Stack m="auto" w={{ base: '100%', xl: '80%' }}>
-        <Title isSimplified={isSimplified} setIsSimplified={setIsSimplified} />
-        <Stack direction="row" justify="space-between" wrap={{ base: 'wrap', md: 'nowrap' }}>
-          <DataContextProvider data={data?.data}>
-            <SelectionContextProvider selectedProvince={selectedProvince}>
-              <Map setSelectedProvince={setSelectedProvince} />
-              <Data isSimplified={isSimplified} />
-            </SelectionContextProvider>
-          </DataContextProvider>
-        </Stack>
+    <Stack m="auto" w={{ base: '100%', xl: '80%' }}>
+      <Title isSimplified={isSimplified} setIsSimplified={setIsSimplified} />
+      <Stack direction="row" justify="space-between" wrap={{ base: 'wrap', md: 'nowrap' }}>
+        <DataContextProvider data={data?.data}>
+          <SelectionContextProvider selectedProvince={selectedProvince}>
+            <Map setSelectedProvince={setSelectedProvince} />
+            <Data isSimplified={isSimplified} />
+          </SelectionContextProvider>
+        </DataContextProvider>
       </Stack>
-    </>
+    </Stack>
   );
 };
 
