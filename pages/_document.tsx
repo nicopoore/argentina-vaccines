14df7de6a36c5667ddba0a67900e03989be9dbcb
Nicolás Poore <nicolaspoore@gmail.com>
2021-03-11 23:04:59 -0300

    Meta tag added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,14 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+import { Meta } from '../components';
 import theme from '../styles/theme';
 
 class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html lang="en">
+        <Meta />
         <Head />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
