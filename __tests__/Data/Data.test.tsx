c19dfecd7fd2c38df20606ce546d62487e052f74
Nicolás Poore <nicolaspoore@gmail.com>
2021-03-18 21:05:41 -0300

    Data test needs fixing

---
+++ __tests__/Data/Data.test.tsx
@@ -0,0 +1,65 @@
+import '@testing-library/jest-dom/extend-expect';
+import { RenderResult, render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import { Data } from '../../components';
+import NumbersSection from '../../components/Data/NumbersSection';
+import { DataContextProvider, SelectionContextProvider } from '../../utils/Context';
+import {
+  rawData,
+  firstDoseNumbers,
+  firstDosePercentages,
+  secondDoseNumbers,
+  secondDosePercentages,
+} from '../../__mocks__/data/dataMock.json';
+import provinceNames from '../../__mocks__/provinceNames';
+import '../../__mocks__/matchMedia.mock';
+import { cache, SWRConfig } from 'swr';
+
+const renderWithContexts = (ui: JSX.Element, selectedProvince: string): RenderResult => {
+  return render(
+    <SelectionContextProvider selectedProvince={selectedProvince}>
+      <SWRConfig value={{ dedupingInterval: 0 }}>{ui}</SWRConfig>
+    </SelectionContextProvider>
+  );
+};
+
+afterEach(async () => {
+  cache.clear();
+});
+
+provinceNames.map(provinceName => {
+  it(`renders ${provinceName} simplified without crashing`, () => {
+    renderWithContexts(<Data isSimplified testing />, provinceName);
+  });
+
+  it(`renders ${provinceName} simplified loading state correctly`, () => {
+    renderWithContexts(<Data isSimplified testing />, provinceName);
+
+    expect(screen.getByRole('heading', { name: provinceName })).toBeInTheDocument();
+    expect(screen.getAllByTestId('vaccineNumbersSkeleton')).toHaveLength(4);
+    const buttonTag = new RegExp(`vacunación en ${provinceName}`);
+    expect(screen.getByText(buttonTag)).toBeInTheDocument();
+    if (provinceName === 'Tierra del Fuego') {
+      expect(screen.getByRole('button', { name: /conocé más/i })).toBeInTheDocument();
+    } else {
+      expect(screen.getByRole('link', { name: /conocé más/i })).toBeInTheDocument();
+    }
+  });
+
+  // FIXME
+
+  it(`renders ${provinceName} simplified loaded state correctly`, async () => {
+    renderWithContexts(<Data isSimplified testing />, provinceName);
+
+    const personNumbers = await waitFor(() =>
+      screen.getAllByRole('heading', { name: /^\d+(\.\d+)*$/ })
+    );
+    expect(personNumbers).toHaveLength(2);
+    expect(personNumbers[0].nextSibling).toHaveTextContent(/personas/i);
+    expect(personNumbers[1].nextSibling).toHaveTextContent(/personas/i);
+    const percentageNumbers = await waitFor(() =>
+      screen.getAllByRole('heading', { name: /\\d+(?:\\,\\d+)? %/ })
+    );
+    expect(percentageNumbers).toHaveLength(2);
+  });
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
 // Dependencies
 import React, { useEffect, useState } from 'react';
-import { Stack, Text } from '@chakra-ui/react';
-import useSWR, { mutate } from 'swr';
+import { Stack } from '@chakra-ui/react';
+import { mutate } from 'swr';
 
 // Components
 import { Map, Data, Title } from '../components';
 
 // Utils
-import { fetcher, postCurrentData } from '../utils/functions';
-import { SelectionContextProvider, DataContextProvider } from '../utils/Context';
+import { postCurrentData } from '../utils/functions';
+import { SelectionContextProvider } from '../utils/Context';
 
 const Home = (): JSX.Element => {
   const [selectedProvince, setSelectedProvince] = useState('Argentina');
   const [isSimplified, setIsSimplified] = useState(true);
-
-  const { data, error } = useSWR('/api/data', fetcher);
 
   useEffect(() => {
     const postDataAndMutate = async (): Promise<void> => {
@@ -25,33 +23,14 @@ const Home = (): JSX.Element => {
     postDataAndMutate();
   }, []);
 
-  if (error)
-    return (
-      <Stack m="auto" w={{ base: '100%', xl: '80%' }}>
-        <Title isSimplified={isSimplified} setIsSimplified={setIsSimplified} />
-        <Stack direction="row" wrap={{ base: 'wrap', md: 'nowrap' }}>
-          <SelectionContextProvider selectedProvince={selectedProvince}>
-            <Map setSelectedProvince={setSelectedProvince} />
-          </SelectionContextProvider>
-          <Stack alignItems="center" flexGrow={1} justify="center">
-            <Text>Error al buscar datos</Text>
-            <Text>Error: {error.name}</Text>
-            <Text>Mensaje: {error.message}</Text>
-          </Stack>
-        </Stack>
-      </Stack>
-    );
-
   return (
     <Stack m="auto" w={{ base: '100%', xl: '80%' }}>
       <Title isSimplified={isSimplified} setIsSimplified={setIsSimplified} />
       <Stack direction="row" justify="space-between" wrap={{ base: 'wrap', md: 'nowrap' }}>
-        <DataContextProvider data={data?.data}>
-          <SelectionContextProvider selectedProvince={selectedProvince}>
-            <Map setSelectedProvince={setSelectedProvince} />
-            <Data isSimplified={isSimplified} />
-          </SelectionContextProvider>
-        </DataContextProvider>
+        <SelectionContextProvider selectedProvince={selectedProvince}>
+          <Map setSelectedProvince={setSelectedProvince} />
+          <Data isSimplified={isSimplified} />
+        </SelectionContextProvider>
       </Stack>
     </Stack>
   );
