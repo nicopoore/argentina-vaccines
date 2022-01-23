d4594c4e5396041f94a321f471319bc4fbdb0ab8
Nicolás Poore <nicolaspoore@gmail.com>
2022-01-23 20:41:39 -0300

    fix: next build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 import theme from '../styles/theme';
 import Head from 'next/head';
+import { AppProps } from 'next/dist/shared/lib/router/router';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
